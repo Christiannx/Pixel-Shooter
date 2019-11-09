726ea8daac2a98025ff0e13d8fa2fc8253fde4b1
Christian <57574969+Christiannx@users.noreply.github.com>
2019-11-10 00:18:23 +0100

    Restored Previous Project

---
+++ Pixel Shooter/Assets/Player/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Vector3 offset;
+    Transform target;
+    
+    void Start() => target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    void Update() {
+        transform.position = target.position + offset;
+        transform.LookAt(target);
+    }
+}
---
+++ Pixel Shooter/Assets/Player/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed;
+
+    new Rigidbody rigidbody;
+    Vector3 movement;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.deltaTime);
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            var lookAt = new Vector3() {
+                x = hit.point.x,
+                y = transform.position.y,
+                z = hit.point.z
+            };
+            transform.LookAt(lookAt);
+        }
+    }
+}
